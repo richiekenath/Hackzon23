3f38adcbd9dab0b8d66716e31b34bb907a211e6e
Richie Kenath Collin R <93726104+richiekenath@users.noreply.github.com>
2023-01-06 12:52:09 +0530

    Add files via upload

---
+++ code/constants/index.js
@@ -0,0 +1,15 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/',
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/',
+    disabled: true,
+  },
+];
